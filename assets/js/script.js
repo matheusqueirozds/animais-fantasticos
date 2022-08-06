46af56c0525bda655316889a13938e4bedd95688
Matheus Queiroz dos Santos <matheusqueirozds@gmail.com>
2022-08-06 16:03:06 -0300

    Atualizei as informações dos animais no html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,25 @@
-const tabMenu = document.querySelectorAll(".js-tabmenu li");
+function initTabNav() {
+    const tabMenu = document.querySelectorAll(".js-tabmenu li");
 
-const tabContent = document.querySelectorAll(".js-tabcontent section");
+    const tabContent = document.querySelectorAll(".js-tabcontent section");
 
-if (tabMenu.length && tabContent.length) {
-    tabContent[0].classList.add("ativo");
+    if (tabMenu.length && tabContent.length) {
+        tabContent[0].classList.add("ativo");
 
-    function activeTab(index) {
-        tabContent.forEach((section) => {
-            section.classList.remove("ativo");
+        function activeTab(index) {
+            tabContent.forEach((section) => {
+                section.classList.remove("ativo");
+            });
+
+            tabContent[index].classList.add("ativo");
+        }
+
+        tabMenu.forEach((li, index) => {
+            li.addEventListener("click", () => {
+                activeTab(index);
+            });
         });
-
-        tabContent[index].classList.add("ativo");
     }
-
-    tabMenu.forEach((li, index) => {
-        li.addEventListener("click", () => {
-            activeTab(index);
-        });
-    });
 }
 
+initTabNav();
