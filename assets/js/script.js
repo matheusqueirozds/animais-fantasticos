e861767ee215b780b75e2c8809ae6e2dfbb248b7
Matheus Queiroz dos Santos <matheusqueirozds@gmail.com>
2022-08-06 17:16:03 -0300

    Adicionei o js no faq

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,19 @@
+const activeClass = "ativo";
+
 function initTabNav() {
     const tabMenu = document.querySelectorAll(".js-tabmenu li");
 
     const tabContent = document.querySelectorAll(".js-tabcontent section");
 
     if (tabMenu.length && tabContent.length) {
-        tabContent[0].classList.add("ativo");
+        tabContent[0].classList.add(activeClass);
 
         function activeTab(index) {
             tabContent.forEach((section) => {
-                section.classList.remove("ativo");
+                section.classList.remove(activeClass);
             });
 
-            tabContent[index].classList.add("ativo");
+            tabContent[index].classList.add(activeClass);
         }
 
         tabMenu.forEach((li, index) => {
@@ -21,5 +23,25 @@ function initTabNav() {
         });
     }
 }
-
 initTabNav();
+
+function initAccordion() {
+    const accordionList = document.querySelectorAll(".js-accordion dt");
+
+    if (accordionList.length) {
+        accordionList[0].classList.add(activeClass);
+
+        accordionList[0].nextElementSibling.classList.add(activeClass);
+
+        function activeAccordion(e) {
+            this.classList.toggle(activeClass);
+
+            this.nextElementSibling.classList.toggle(activeClass);
+        }
+
+        accordionList.forEach((item) => {
+            item.addEventListener("click", activeAccordion);
+        });
+    }
+}
+initAccordion();
