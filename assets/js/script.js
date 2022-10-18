6667c10450e97ea34ed0a37662e89b09677c1468
Matheus Queiroz dos Santos <matheusqueirozds@gmail.com>
2022-10-17 23:36:38 -0300

    ♻️ refactor: Animações iniciais criadas e refatoração para sass

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,17 @@
-const active = "ativo";
+const activeClass = "ativo";
 
 const initTabNav = () => {
   const tabMenu = document.querySelectorAll(".js-tabmenu li");
   const tabContent = document.querySelectorAll(".js-tabcontent section");
 
   if (tabMenu.length && tabContent) {
-    tabContent[0].classList.add(active);
+    tabContent[0].classList.add(activeClass);
 
     const activeTab = (index) => {
       tabContent.forEach((section) => {
-        section.classList.remove(active);
+        section.classList.remove(activeClass);
       });
-      tabContent[index].classList.add(active);
+      tabContent[index].classList.add(activeClass);
     };
 
     tabMenu.forEach((li, index) => {
@@ -23,13 +23,70 @@ const initTabNav = () => {
 };
 initTabNav();
 
-const accordionList = document.querySelectorAll(".js-accordion dt");
+const initActiveAccordion = () => {
+  const accordionList = document.querySelectorAll(".js-accordion dt");
 
-function activeAccordion() {
-  this.classList.toggle(active);
-  this.nextElementSibling.classList.toggle(active);
-}
+  if (accordionList.length) {
+    accordionList[0].classList.add(activeClass);
+    accordionList[0].nextElementSibling.classList.add(activeClass);
 
-accordionList.forEach((dt) => {
-  dt.addEventListener("click", activeAccordion);
-});
+    function activeAccordion() {
+      this.classList.toggle(activeClass);
+      this.nextElementSibling.classList.toggle(activeClass);
+    }
+
+    accordionList.forEach((dt) => {
+      dt.addEventListener("click", activeAccordion);
+    });
+  }
+};
+initActiveAccordion();
+
+const initSmoothScroll = () => {
+  const internalLinks = document.querySelectorAll(".js-menu a[href^='#']");
+
+  function scrollToSection(e) {
+    e.preventDefault();
+    const href = this.getAttribute("href");
+    const section = document.querySelector(href);
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+
+    // Forma alternativa:
+    /* const top = section.offsetTop;
+    window.scrollTo({
+    top: top,
+    behavior: "smooth"
+    }); */
+  }
+
+  internalLinks.forEach((link) => {
+    link.addEventListener("click", scrollToSection);
+  });
+};
+initSmoothScroll();
+
+const initVisibleSections = () => {
+  const sections = document.querySelectorAll(".js-scroll");
+
+  if (sections.length) {
+    const halfWindow = window.innerHeight * 0.5;
+
+    const animatedScroll = () => {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = sectionTop - halfWindow < 0;
+        if (isSectionVisible) {
+          section.classList.add(activeClass);
+        }
+      });
+    };
+
+    animatedScroll();
+
+    window.addEventListener("scroll", animatedScroll);
+  }
+};
+initVisibleSections();
